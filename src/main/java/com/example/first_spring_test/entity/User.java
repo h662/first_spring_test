695150bf77c6592880c2168ff8ad1eca2cd3fac5
h662 <h662hong@gmail.com>
2025-09-30 17:04:11 +0900

    Initial commit

---
+++ src/main/java/com/example/first_spring_test/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.first_spring_test.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
